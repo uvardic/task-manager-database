2b5eed7f16df1c0cbf965340991ca1d97bea5ef9
uvardic <uros.vardiic@gmail.com>
2020-07-05 12:40:22 +0200

    Database configuration

---
+++ src/main/java/taskmanager/database/config/DatabaseConfiguration.java
@@ -0,0 +1,13 @@
+package taskmanager.database.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EntityScan("taskmanager.database")
+@EnableJpaRepositories("taskmanager.database")
+public class DatabaseConfiguration {
+}
