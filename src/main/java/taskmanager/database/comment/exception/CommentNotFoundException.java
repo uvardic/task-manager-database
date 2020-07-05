e335d13446b0002e767042ade999d3c711e801fd
Uros Vardic <uros.vardiic@gmail.com>
2020-07-05 19:50:02 +0200

    Comment CRUD operations

---
+++ src/main/java/taskmanager/database/comment/exception/CommentNotFoundException.java
@@ -0,0 +1,9 @@
+package taskmanager.database.comment.exception;
+
+public class CommentNotFoundException extends RuntimeException {
+
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/taskmanager/database/comment/service/CommentService.java
@@ -0,0 +1,63 @@
+package taskmanager.database.comment.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import taskmanager.database.comment.domain.Comment;
+import taskmanager.database.comment.dto.CommentDTO;
+import taskmanager.database.comment.exception.CommentNotFoundException;
+import taskmanager.database.comment.mapper.CommentMapper;
+import taskmanager.database.comment.repository.CommentRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository repository;
+
+    private final CommentMapper mapper;
+
+    @Transactional
+    public void deleteById(Long existingId) {
+        if (commentNotFound(existingId))
+            throw new CommentNotFoundException(String.format("Comment with id: %d, was't found!", existingId));
+
+        repository.deleteById(existingId);
+    }
+
+    private boolean commentNotFound(Long existingId) {
+        return repository.findById(existingId).isEmpty();
+    }
+
+    @Transactional
+    public CommentDTO save(CommentDTO request) {
+        Comment savedEntity = repository.save(mapper.toEntity(request));
+        return mapper.toDTO(savedEntity);
+    }
+
+//    no need for an update method at this time
+//    @Transactional
+//    public CommentDTO update(Long existingId, CommentDTO request) {
+//        CommentDTO foundComment = findById(existingId);
+//        // setters
+//        Comment savedEntity = repository.save(mapper.toEntity(foundComment));
+//        return mapper.toDTO(savedEntity);
+//    }
+
+    public CommentDTO findById(Long existingId) {
+        return repository.findById(existingId)
+                .map(mapper::toDTO)
+                .orElseThrow(() -> new CommentNotFoundException(
+                        String.format("Comment with id: %d, wasn't found!", existingId)
+                ));
+    }
+
+    public List<CommentDTO> findAll() {
+        return mapper.toDTOList(repository.findAll());
+    }
+
+}
