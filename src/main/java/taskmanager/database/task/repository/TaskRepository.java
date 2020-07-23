d42e3ebdfcb8316ab27e2d4a701a21c6864f0865
Uros Vardic <uros.vardiic@gmail.com>
2020-07-23 17:22:01 +0200

    Task find by id query

--- src/main/java/taskmanager/database/task/repository/TaskRepository.java
+++ src/main/java/taskmanager/database/task/repository/TaskRepository.java
@@ -1,14 +1,25 @@
 package taskmanager.database.task.repository;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import taskmanager.database.task.domain.Task;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    @NotNull
+    @Query(
+            "SELECT task FROM Task task " +
+            "JOIN FETCH task.section section " +
+            "JOIN FETCH section.project " +
+            "WHERE task.id = :id"
+    )
+    Optional<Task> findById(@NotNull Long id);
 
     @Query(
             "SELECT task FROM Task task " +
